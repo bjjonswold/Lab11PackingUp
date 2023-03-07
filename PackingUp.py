1517d6d9306a87e6c9c3fa13f5ad4e9635916a41
bjjonswold <bjjonswold@gmail.com>
2023-03-07 00:49:13 -0700

    Participation answers

--- PackingUp.py
+++ PackingUp.py
@@ -1,3 +1,21 @@
+'''
+1. The variables at the top are to initialize objects that will be 
+used to store lists later on.
+2. It will go through a max of two other functions before returning to Main
+3. SortList(boxes) uses another funciton putAway() to place the box in the
+correct location. 
+4. This declaration is stating what the location variable is equal too in the
+function call itself, instead of calling it from elsewhere. 
+5. Some uses of defaults include:
+    A. Avoiding errors - a function will always have a parameter
+    work with, even if it isn't declared in the call
+    B. Simplifies calling arguments - you don't need to call the same
+    parameter every time
+You can call this funciton with the default by writing:
+someFunction(arg1) 
+'''
+
+
 import csv
 
 # What are these for?
